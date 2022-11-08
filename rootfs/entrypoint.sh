9999ab2103a8611023c10404e60ce905893d0a99
Socheat Sok <alex@socheat.net>
2022-11-08 18:30:31 +0700

    Update rootfs/entrypoint.sh

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -7,9 +7,9 @@ PB_PORT=${PB_PORT:-8090}
 PB_DATA_DIR="${PB_DATA_DIR:-/opt/pocketbase}"
 PB_PUBLIC_DIR="${PB_PUBLIC_DIR:-/var/pocketbase/public}"
 
-test -n "${PB_DEBUG}" && POCKETBASE_FLAGS+=("--debug")
-test -n "${PB_CORS}" && POCKETBASE_FLAGS+=("--origins=${PB_CORS}")
-test -n "${PB_ENCRYPTION_KEY}" && POCKETBASE_FLAGS+=('--encryptionEnv=PB_ENCRYPTION_KEY')
+test -n "${PB_DEBUG}"           && POCKETBASE_FLAGS+=("--debug")
+test -n "${PB_CORS}"            && POCKETBASE_FLAGS+=("--origins=${PB_CORS}")
+test -n "${PB_ENCRYPTION_KEY}"  && POCKETBASE_FLAGS+=('--encryptionEnv=PB_ENCRYPTION_KEY')
 
 # Print banner and version
 cat /var/pocketbase/banner
